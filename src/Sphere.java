09bd01a9f0b1e908d2dc74170619c0c8c4e2c79e
Karthik Sharma <Karthik Sharma>
2017-01-27 20:22:35 +0530

    corrected syntax errors

--- src/Sphere.java
+++ src/Sphere.java
@@ -12,7 +12,7 @@ class Sphere implements Entity {
         this.radius = conf.getDouble("radius");
     }
 
-    public double getMin(int x, int y) {
+    private double getMin(double x, double y) {
         if (x<y)
             return x;
         return y;
@@ -20,27 +20,26 @@ class Sphere implements Entity {
 
     public Vector getIntersectionPoint(Ray ray) {
         double a = 1.0;
-        double b = 2( ray.getDirection().getX()*(ray.getOrigin().getX() - center.getX()) +
-                ray.getDirection().getY()*(ray.getOrigin().getY() - center.getY()) +
-                ray.getDirection().getZ()*(ray.getOrigin().getZ() - center.getZ()) );
-        double c = (ray.getOrigin().getX() - center.getX())*(ray.getOrigin().getX() - center.getX())
-                + (ray.getOrigin().getY() - center.getY())*(ray.getOrigin().getY() - center.getY())
-                + (ray.getOrigin().getZ() - center.getZ())*(ray.getOrigin().getZ() - center.getZ()) ;
+        double b = 2.0 * ( ray.getDirection().getX()*(ray.getOrigin().getX() - centre.getX()) +
+                ray.getDirection().getY()*(ray.getOrigin().getY() - centre.getY()) +
+                ray.getDirection().getZ()*(ray.getOrigin().getZ() - centre.getZ())) ;
+        double c = (ray.getOrigin().getX() - centre.getX())*(ray.getOrigin().getX() - centre.getX())
+                + (ray.getOrigin().getY() - centre.getY())*(ray.getOrigin().getY() - centre.getY())
+                + (ray.getOrigin().getZ() - centre.getZ())*(ray.getOrigin().getZ() - centre.getZ()) ;
         double t0 = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
         double t1 = (-b - Math.sqrt(b*b - 4*a*c))/(2*a);
 
         double t = getMin(t0,t1);
 
-        Vector intersection = new Vector(ray.getOrigin().getX() + ray.getDirection().getX()*t,
+        return new Vector(ray.getOrigin().getX() + ray.getDirection().getX()*t,
                 ray.getOrigin().getY() + ray.getDirection().getY()*t,
                 ray.getOrigin().getZ() + ray.getDirection().getZ()*t);
-        return intersection;
     }
 
     public Vector getNormal(Ray ray) {
-        return new Vector ( (getIntersection(ray) - center.getX())/radius,
-                (getIntersection(ray) - center.getY())/radius,
-                (getIntersection(ray) - center.getZ())/radius );
-    },
+        return new Vector ( (getIntersection(ray) - centre.getX())/radius,
+                (getIntersection(ray) - centre.getY())/radius,
+                (getIntersection(ray) - centre.getZ())/radius );
+    }
 }
 
